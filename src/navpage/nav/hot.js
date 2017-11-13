131bf27b722292cde52298b14ea044fe62e742c3
shengchao <749312025@qq.com>
2017-11-13 18:26:59 +0800

    redux demo test and study

--- src/navpage/nav/hot.js
+++ src/navpage/nav/hot.js
@@ -7,11 +7,13 @@ import {
   View
 } from 'react-native'
 
+import App from '../../counter/containers/app'
+
 export default class NewBar extends Component {
   render() {
     return (
       <View style={{flex:1}}>
-        <Text>hot</Text>
+        <App />
       </View>
     )
   }
