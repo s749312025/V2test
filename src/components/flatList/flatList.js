4f136f94aba69d54df4a537e079ea0a2ec042ff8
s749312025 <749312025@qq.com>
2017-11-17 09:01:23 -0800

    add item style & moment

--- src/components/flatList/flatList.js
+++ src/components/flatList/flatList.js
@@ -9,6 +9,10 @@ import {
   View
 } from 'react-native'
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import moment from 'moment'
+
 class ListItem extends Component {
   constructor(props) {
     super(props);
@@ -19,9 +23,19 @@ class ListItem extends Component {
     let image = 'https:' + item.member.avatar_normal
     return(
       <View style={styles.item}>
-        <View>
+        <View style={styles.ceater}>
           <Image style={styles.img} source={{uri:image}} />
-          <Text>{item.member.username}</Text>
+          <View style={styles.attach}>
+            <View style={styles.firstline}>
+              <Text>{item.member.username}</Text>
+              <View style={styles.discuss}>
+                <Text style={styles.node}>{item.node.title}</Text>
+                <Icon style={styles.icon} name={ 'comment-text-outline' } size={13} color={'gray'} />
+                <Text style={styles.count}>{item.replies}</Text>
+              </View>
+            </View>
+            <Text style={styles.last}>最后回复：{item.last_touched}</Text>
+          </View>
         </View>
         <Text style={styles.title}>{item.title}</Text>
       </View>
@@ -63,17 +77,53 @@ export default class Scroll extends Component {
 const styles = StyleSheet.create({
   item: {
     backgroundColor: '#fff',
-    padding: 10,
-    marginBottom: 5
+    padding: 12,
+    marginBottom: 8
+  },
+  ceater: {
+    marginBottom: 10,
+    flex: 1,
+    flexDirection: 'row'
   },
   img: {
     height: 35,
     width: 35,
     borderRadius: 3,
-    marginBottom: 10
+  },
+  attach: {
+    marginLeft: 15,
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  last: {
+    fontSize: 12,
+    color: '#aaa'
+  },
+  firstline: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  node: {
+    fontSize: 10,
+    color: '#999',
+    padding: 5,
+    marginRight: 10,
+    backgroundColor: '#eee'
+  },
+  icon: {
+    marginTop: 4,
+    marginRight: 3
+  },
+  discuss: {
+    flexDirection: 'row'
+  },
+  count: {
+    color: '#aaa',
+    marginTop: 2
   },
   title: {
-    fontSize: 15,
+    fontSize: 17,
+    lineHeight: 22,
     color: '#333'
   }
 })
